736f4433e11da03301a99c97a16413b7e2a17e71
Cassiano <Cassianocosta12357@gmail.com>
2026-02-26 00:01:34 -0300

    update

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -11,6 +11,23 @@ export default function Game({ verifyLetter }) {
             <h3 className="tip">
                 Dica sobre a palavra: <span>Dica...</span>
             </h3>
+            <div className="wordContainer">
+                <span className="letter">A</span>
+                <span className="blankSquare"></span>
+            </div>
+            <div className="letterContainer">
+                <p>Tente advinhar uma letra da palavra:</p>
+                <form>
+                    <input type='text' name='letter' maxLength={1} required />
+                    <button>Jogar</button>
+                </form>
+            </div>
+            <div className="wrongLettersContainer">
+                <p>Letras ja utilizadas:</p>
+                <span>a,</span>
+                <span>b,</span>
+            </div>
         </div>
     )
 }
+    
